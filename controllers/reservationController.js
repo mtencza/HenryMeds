c4c3d02e08c6ed56fcd4150394651f0dea22611d
mtencza <mtencza@cs.stonybrook.edu>
2023-08-20 21:19:11 -0400

    reserve/confirm logic

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -1,4 +1,5 @@
 const Reservation = require('../models/reservation');
+const Schedule = require('../models/schedule');
 
 function isValidReservation(start_time) {
     const startTime = new Date(start_time).getTime();
@@ -17,6 +18,38 @@ function withinThirtyMin(time_to_check) {
     return (currentTime - time <= thirtyMin);
 }
 
+async function isReservationInSchedule(providerid, start_time, end_time) {
+    //Get day from start
+    const date = new Date(start_time);
+    const day = date.getDate();
+    const month = date.getMonth() + 1; //0 index so plus one
+    const year = date.getFullYear();
+    const reservationDay = `${year}-${month}-${day}`; //YYYY-MM-DD
+
+    // Get schedule(s) for that provider/day only 
+    const providerSchedules = await Schedule.findAll({
+      where: {
+        providerid: providerid,
+        date: reservationDay,
+      },
+    });
+  
+    const reservationStartTime = new Date(start_time).getTime();
+    const reservationEndTime = new Date(end_time).getTime();
+
+    //Do check to make sure there is a schedule entry that overlaps with res
+    const isValidScheduleEntry = providerSchedules.some(providerSchedule => {
+        const scheduleStartTime = new Date(providerSchedule.start_time).getTime();
+        const scheduleEndTime = new Date(providerSchedule.end_time).getTime();
+        return (reservationStartTime >= scheduleStartTime
+                && reservationEndTime <= scheduleEndTime);
+    });
+  
+    return isValidScheduleEntry;
+}
+
+
+/*   POST /reservations   */
 async function reserve(providerid, clientid, start_time, end_time) {
     try {
         //Reservations must be made at least 24 hours in advance
@@ -24,7 +57,31 @@ async function reserve(providerid, clientid, start_time, end_time) {
             throw new Error('Reservation must be made 24 hrs in advance');
         }
 
-        //Valid provider schedule
+        //check that provider is working during that time
+        if(!isReservationInSchedule(providerid, start_time, end_time)) {
+            throw new Error('Provider is not working at that time');
+        }
+
+        //Check that a reservation isn't already present for the provider for this start and end time
+        const existingReservation = await Reservation.findOne({
+            where: {
+                providerid: providerid,
+                start_time: start_time,
+                end_time: end_time,
+                booked: true,
+            },
+        });
+
+        //do 30 min check and delete existing reservation if expired
+        if(existingReservation) {
+            if(!existingReservation.confirmed && 
+                !withinThirtyMin(existingReservation.creation_time)) { //booked but not confirmed
+                await existingReservation.destroy();
+            }
+            else {
+                throw new Error('Reservation already exists at that time');
+            }
+        }
 
         const newReservation = await Reservation.create({
             providerid: providerid,
@@ -41,7 +98,7 @@ async function reserve(providerid, clientid, start_time, end_time) {
 }
 
 
-
+/*   POST /reservations/confirmation   */
 async function confirm(reservationid) {
     try {
         const existingReservation = await Reservation.findByPk(reservationid); 
@@ -51,11 +108,10 @@ async function confirm(reservationid) {
 
         //Reservations expire after 30 mins if not confirmed
         if(!withinThirtyMin(existingReservation.creation_time)) {
-            //delete reservation
+            //delete existingg invalid reservation
             await existingReservation.destroy();
             throw new Error('Reservation must be confirmed within 30 minutes');
         }
-
   
         existingReservation.confirmed = true;
         await existingReservation.save();
