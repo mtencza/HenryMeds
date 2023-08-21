9cf11f1b27e44eaffe6d6d5c3e08c99c421d1cbb
mtencza <mtencza@cs.stonybrook.edu>
2023-08-20 20:46:30 -0400

    Schedule insert

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -4,7 +4,14 @@ const Schedule = require('../models/schedule');
 //reservations/schedule
 async function schedule(providerid, start_time, end_time) {
     try {
-        //insert new schedule into schedules
+      //insert new schedule into schedules
+      const newSchedule = await Schedule.create({
+        providerid: providerid,
+        start_time: start_time,
+        end_time: end_time,
+      });
+      
+      return newSchedule;
     } catch (error) {
         throw error;
     }
