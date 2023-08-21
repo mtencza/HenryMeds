b4388018db7c2db28d00b3ef9b1c882bea5c0c23
mtencza <mtencza@cs.stonybrook.edu>
2023-08-20 20:43:48 -0400

    Schedule controller skeleton

---
+++ controllers/scheduleController.js
@@ -0,0 +1,15 @@
+const Schedule = require('../models/schedule');
+
+
+//reservations/schedule
+async function schedule(providerid, start_time, end_time) {
+    try {
+        //insert new schedule into schedules
+    } catch (error) {
+        throw error;
+    }
+}
+
+module.exports = {
+    schedule,
+};
