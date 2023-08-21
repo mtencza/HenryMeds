1d93b6d6eb47e11ef3b683a65d4461953423e087
mtencza <mtencza@cs.stonybrook.edu>
2023-08-20 20:50:06 -0400

    Schedule day field

--- controllers/scheduleController.js
+++ controllers/scheduleController.js
@@ -4,11 +4,20 @@ const Schedule = require('../models/schedule');
 //reservations/schedule
 async function schedule(providerid, start_time, end_time) {
     try {
+
+      //Get day from start
+      const date = new Date(start_time);
+      const day = date.getDate();
+      const month = date.getMonth() + 1; //0 index so plus one
+      const year = date.getFullYear();
+      const scheduleDay = `${year}-${month}-${day}`; //YYYY-MM-DD
+      
       //insert new schedule into schedules
       const newSchedule = await Schedule.create({
         providerid: providerid,
         start_time: start_time,
         end_time: end_time,
+        date: scheduleDay,
       });
       
       return newSchedule;
